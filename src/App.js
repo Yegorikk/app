8b883fcaa70d950dac3c13040f71593c2b1dffed
Yegorikk <0yegorstep0@gmail.com>
2023-06-16 12:42:00 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import store from './store';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function Button(props) {
+  return <button
+    onClick={() => { props.trigger(props.val) }}
+  >{props.val}</button>
 }
 
-export default App;
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      out: "0"
+    }
+    this.refOutput = React.createRef()
+  }
+
+  enterNumber(value) {
+    let currentValue = value;
+    let output = this.refOutput.current
+    this.setState({
+      out: currentValue
+    })
+    if (output.value === '0') { output.value = '' }
+    output.value += currentValue
+  }
+
+  enterOperation(value) {
+    let currentValue = value;
+    let output = this.refOutput.current
+    if (value === '+' || value === '-' || value === '*' || value === '/') { output.value += currentValue }
+    if (value === 'C') { output.value = '0' }
+    if (value === 'CE') {
+      output.value = output.value.substring(0, output.value.length - 1)
+      if (output.value === '') { output.value = '0' }
+    }
+    if (value === '=') {
+      try {
+        output.value = eval(output.value)
+      }
+      catch {
+        output.value = "Incorrect data"
+        setTimeout(() => {
+          output.value = '0'
+        }, 2000)
+      }
+    }
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div className="output">
+          <input ref={this.refOutput} type="text" defaultValue={this.state.out} />
+        </div>
+        <div className='buttons-hold'>
+          <div className="buttons">
+            {store.buttons.map(item => <Button
+              key = {item.val}
+              trigger = {this.enterNumber.bind(this)}
+              val = {item.val}
+            >{item.val}</Button>)}
+          </div>
+          <div className="operations-column">
+            {store.operations_column.map(item => <button
+              key = {item.val}
+              onClick={() => { this.enterOperation(item.val) }}
+            >{item.val}</button>)}
+          </div>
+        </div>
+        <div className="operations-row">
+          {store.operations_row.map(item => <button
+            key = {item.val}
+            onClick={() => { this.enterOperation(item.val) }}
+          >{item.val}</button>)}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ src/store.js
@@ -0,0 +1,13 @@
+let store = { 
+    buttons: [
+        {val: "1"}, {val: "2"}, {val: "3"}, {val: "4"}, {val: "5"}, {val: "6"}, {val: "7"}, {val: "8"}, {val: "9"}, {val: "0"}
+    ],
+    operations_column: [
+        {val:"+"}, {val:"-"}, {val: "*"}, {val: "/"}
+    ],
+    operations_row: [
+        {val:"CE"}, {val:"C"}, {val: "="}
+    ]
+}
+
+export default store
